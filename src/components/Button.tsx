b2481912b62aa74085f009bb639fb17b14a20211
hee-develop <hee950320@yahoo.co.jp>
2021-07-29 00:28:56 +0900

    add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import styles from '../styles/button.module.scss';
+
+type Props = {
+  text: string;
+  description?: string;
+  accent?: boolean;
+  onClicked?: () => void;
+}
+
+export default function Button({
+  text,
+  description,
+  accent,
+  onClicked
+}: Props) {
+  const buttonClass = accent ? styles.buttonAccent : styles.button;
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={onClicked}
+      title={description}
+    >
+      {text}
+    </button>
+  );
+}
