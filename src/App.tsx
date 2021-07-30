a54f81a9e384aac69f9422fb03e27ecbad2200b3
hee-develop <hee950320@yahoo.co.jp>
2021-07-30 22:18:10 +0900

    add AddModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import Memo from './components/Memo';
+import AddModal from './components/AddModal';
+
+import { attachTargetToArray, removeTargetFromArray } from './functions/modifyArray';
+
 
 function App() {
+  const [memos, setMemos] = useState(['aple', 'bana', 'carr']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddModal
+        onConfirmed={(str) => setMemos(prev => attachTargetToArray(prev, str))}
+        onCanceled={() => null}
+      />
     </div>
   );
 }
