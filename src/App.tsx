680163ae269d132986389c25d816b0ab3f50f941
hee-develop <hee950320@yahoo.co.jp>
2021-07-30 22:27:10 +0900

    show sample memos

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,34 @@ import './App.scss';
 import Memo from './components/Memo';
 import AddModal from './components/AddModal';
 
-import { attachTargetToArray, removeTargetFromArray } from './functions/modifyArray';
+import { removeTargetFromArray } from './functions/modifyArray';
 
 
 function App() {
   const [memos, setMemos] = useState(['aple', 'bana', 'carr']);
+  const title = 'Simple memo';
+
   return (
     <div className="App">
+      <h1>{title}</h1>
+      <div>
+        {memos.map((memo, idx) => (
+          <>
+            <Memo
+              key={`${idx}-${memo}`}
+              content={memo}
+              onClicked={() => null}
+              onDoubleClicked={() => {
+                setMemos(prev => removeTargetFromArray(prev, idx))
+              }}
+            />
+            {idx !== memos.length - 1 && <>|</>}
+          </>
+        ))}
+      </div>
+
       <AddModal
-        onConfirmed={(str) => setMemos(prev => attachTargetToArray(prev, str))}
+        onConfirmed={(str) => setMemos(prev => [...prev, str])}
         onCanceled={() => null}
       />
     </div>
