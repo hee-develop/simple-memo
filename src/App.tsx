66d49f7070f932ee8b9367d2a9e238944f78c4b9
hee-develop <hee950320@yahoo.co.jp>
2021-08-08 23:00:32 +0900

    save memo data in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import AddModal from './components/AddModal';
 import MemoList from './components/MemoList';
@@ -12,10 +12,29 @@ function App() {
     { content: 'make simple memo', state: 'asap' },
     { content: 'learn react', state: 'normal' },
   ]);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem('simple-memo-serialized-json');
+
+    if (!savedData) return;
+
+    setMemos(JSON.parse(savedData))
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('simple-memo-serialized-json', JSON.stringify(memos));
+  }, [memos]);
+
+  const onBackgroundClicked = useCallback(ev => {
+    if ((ev.target as HTMLDivElement).className !== 'App') return;
+
+    setIsAddModalVisible(true);
+  }, []);
+
   const title = 'Simple memo';
 
   return (
-    <div className="App" onClick={() => setIsAddModalVisible(true)}>
+    <div className="App" onClick={onBackgroundClicked}>
       <h1>{title}</h1>
 
       <MemoList
